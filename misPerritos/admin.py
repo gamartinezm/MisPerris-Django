79712259ed599288b731468f70436fabebe29a5c
GABRIEL MARTINEZ <ga.martinezm@alumnos.duoc.cl>
2018-10-24 13:59:30 -0400

    MisPerris con Django

---
+++ misPerritos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import RescatadosPost
+
+admin.site.register(RescatadosPost)
+
+# Register your models here.
---
+++ misPerritos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class RescatadosPost(models.Model):
+    fotografia = models.ImageField(upload_to='Foto')
+    nombre = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=300)
+    ESTADOS = ((1, "Rescatado"),(2,'Dsiponible'),(3,'Adoptado'),(4,'Seleccionar'))
+    estado = models.IntegerField(choices=ESTADOS,default=4)
+
+
+# Create your models here.
